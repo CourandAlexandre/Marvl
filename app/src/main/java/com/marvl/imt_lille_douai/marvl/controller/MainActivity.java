d92a84ee36fb6f2ae531799bd5593c7ed527dcff
unknown <acourand@etu.imt-lille-douai.fr>
2017-09-29 22:21:12 +0200

    first commit

---
+++ app/src/main/java/com/marvl/imt_lille_douai/marvl/controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.marvl.imt_lille_douai.marvl.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marvl.imt_lille_douai.marvl.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
