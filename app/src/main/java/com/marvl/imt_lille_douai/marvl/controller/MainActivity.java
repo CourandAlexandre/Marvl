7a8963cd183a677c2a7624d8882f35c214b34e2a
FLORENT MARCAILLE <florent.marcaille@atos.net>
2017-11-13 16:14:32 +0100

    Begin photo taking | still to debeug

--- app/src/main/java/com/marvl/imt_lille_douai/marvl/controller/MainActivity.java
+++ app/src/main/java/com/marvl/imt_lille_douai/marvl/controller/MainActivity.java
@@ -4,13 +4,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,6 +24,9 @@ import com.marvl.imt_lille_douai.marvl.R;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -38,6 +45,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     ImageView photoView;
 
+    String photoTakenPath;
+    Uri photoTakenUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +63,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         analyseButton.setOnClickListener(this);
 
         photoView = (ImageView) findViewById(R.id.imageAnalysed);
-        //photoView.setImageURI();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == 0){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED ){
+                captureButton.setEnabled(true); // If user doesn't have the permission the button is hidden
+            }
+        }
     }
 
     public void onClick(View view){
@@ -68,10 +86,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent){
-        if(requestCode == libraryActivityResult && resultCode == Activity.RESULT_OK){
-            Uri photoUri = intent.getData();
-            photoView.setImageURI(photoUri);
-            //Log.i(TAG,photoUri.toString());
+        switch (resultCode){
+            case Activity.RESULT_OK :
+                switch (requestCode){
+                    case captureActivityResult :
+                        photoView.setImageURI(photoTakenUri);
+                    break;
+
+                    case libraryActivityResult :
+                        processPhotoLibrary(intent);
+
+                        Log.i(TAG,intent.toString());
+                    break;
+
+                    case analyseActivityResult :
+                        Uri photoUri = intent.getData();
+                        photoView.setImageURI(photoUri);
+
+                        Log.i(TAG,photoUri.toString());
+                    break;
+                }
+            break;
         }
     }
 
@@ -80,19 +115,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String pathToPhoto = getRealPath(getApplicationContext(),photoUri);
 
         File pathToFile = new File(pathToPhoto);
-        Bitmap photoBitmap = decodeFile(pathToFile);
+        Bitmap photoBitmap = decodeFile(pathToFile); // err -> Maybe on path
+
         photoView.setImageBitmap(photoBitmap);
 
-        //Log.i(TAG,pathToPhoto);
+        Log.i(TAG,pathToPhoto);
     }
 
     protected void startCaptureActivity(){
-        Intent intent = new Intent();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // Ensure that there's a camera activity to handle the intent
+        if(intent.resolveActivity(getPackageManager()) != null){
+            File photoFile = null; // Create the File where the photo should go
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        startActivityForResult(intent,captureActivityResult);
+            // Continue only if the File was successfully created
+            if(photoFile != null){
+                Uri photoUri = FileProvider.getUriForFile(this,"com.marvl.imt_lille_douai.marvl.fileprovider",photoFile);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+                startActivityForResult(intent, captureActivityResult);
+            }
+        }
     }
 
     protected void startLibraryActivity(){
@@ -116,7 +166,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected String getRealPath(Context context, Uri uri) {
         Cursor cursor;
 
-
             String[] projection = {MediaStore.Images.Media.DATA};
             cursor = context.getContentResolver().query(
                 uri,
@@ -136,7 +185,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Bitmap bitmap = null;
 
         try{
-            FileInputStream inputStream = new FileInputStream(file);
+            FileInputStream inputStream = new FileInputStream(file); // System.err
 
             BitmapFactory.Options options = new BitmapFactory.Options();
             bitmap = BitmapFactory.decodeStream(inputStream, null, options);
@@ -145,5 +194,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         return bitmap;
+    }
+
+    // Create an image file name
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+
+        // Save a file : path for use with ACTION_VIEW intents
+        photoTakenPath = image.getAbsolutePath();
+        photoTakenUri = Uri.fromFile(image);
+
+        return image;
+    }
+
+    // Add a photo to a gallery
+    private void galleryAddPic(){
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File file = new File(photoTakenPath);
+        Uri contentUri = Uri.fromFile(file);
+
+        mediaScanIntent.setData(contentUri);
+
+        this.sendBroadcast(mediaScanIntent);
     }
 }
